e0d8d68f6e501f91b11b2efe3aa7a8720df1175c
finnelliott <f.j.elliott@icloud.com>
2021-09-16 23:51:41 +0200

    initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
